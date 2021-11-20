985f86c87a14011e210c899c719144f6ea9fec78
HoaTNNguyen <hoatuongnguyen.nguyen@sjsu.edu>
2021-11-19 19:00:39 -0800

    Autolaunch browser if not in debug mode

--- app/run.py
+++ app/run.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
-
+import threading
+import webbrowser
 from myapp import myapp_obj, db
 
+DEBUG = False
+PORT_NUMBER = 5000
+
+def launch_browser():
+    webbrowser.open(f'http://localhost:{PORT_NUMBER}', new=0)
+
+
+# Create *.db file from schema (if doesn't exists)
 db.create_all()
-myapp_obj.run(debug = True)
+# Launch webbrowser after 1 seconds
+if not DEBUG:
+    threading.Timer(1, launch_browser).start()
+# Run flask app server
+myapp_obj.run(debug=DEBUG, port=PORT_NUMBER)

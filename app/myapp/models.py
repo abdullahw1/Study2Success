48b4945946b2d42613d860aac337686941befd46
HoaTNNguyen <hoatuongnguyen.nguyen@sjsu.edu>
2021-11-25 11:40:58 -0800

    Complete friend system

--- app/myapp/models.py
+++ app/myapp/models.py
@@ -53,3 +53,25 @@ class FlashCard(db.Model):
 #     progress = db.Column(db.Text)
 #     index = db.Column(db.Integer)
 #     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+def get_friend_status(current_user_id, other_user_id):
+    # Get and Check if has pending friend request or already a friend
+    friend_record = db.session.query(Friend)\
+                    .filter(((Friend.user1_id == current_user_id) & (Friend.user2_id == other_user_id))\
+                            | ((Friend.user2_id == current_user_id) & (Friend.user1_id == other_user_id))
+                    ).one_or_none()
+    # If found friend record
+    if friend_record:
+        if friend_record.status == FriendStatusEnum.FRIEND:
+            status = 'friend'
+        elif friend_record.status == FriendStatusEnum.PENDING:
+            if friend_record.user1.id == int(current_user_id): # Current user sent the request
+                status = 'pending-sent-request'
+            else: # The other user sent the request, current user needs to approve
+                status = 'pending-to-approve'
+        else:
+            raise Exception(f"Unknown status {friend_record.status}")
+    else:
+        # No record, not friend/pending, neutral
+        status = 'neutral'
+    return status, friend_record
--- app/myapp/routes.py
+++ app/myapp/routes.py
@@ -6,6 +6,7 @@ from myapp import myapp_obj
 from myapp.forms import SignupForm, LoginForm, FlashCardForm, UploadMarkdownForm, SearchForm
 from myapp import db
 from myapp.models import User, FlashCard, Friend, FriendStatusEnum
+from myapp.models import get_friend_status
 from myapp.mdparser import md2flashcard
 
 @myapp_obj.route("/")
@@ -100,11 +101,28 @@ def import_flashcard():
 @myapp_obj.route("/my-friends", methods=['GET', 'POST'])
 @login_required
 def show_friends():
-    friends = db.session.query(Friend)\
+    friends = []
+    result = db.session.query(Friend)\
                 .filter((Friend.user1_id == current_user.get_id())\
                         | (Friend.user2_id == current_user.get_id())
                 ).all()
-    friends = [(x.user1, x.status) if x.user1.id != int(current_user.get_id()) else (x.user2, x.status) for x in friends]
+    for x in result:
+        if x.user1.id == int(current_user.get_id()):
+            cur_user = x.user1
+            oth_user = x.user2
+        else:
+            cur_user = x.user2
+            oth_user = x.user1
+        if x.status == FriendStatusEnum.FRIEND:
+            buttons = [(f'/remove-friend/{oth_user.id}', 'Remove Friend')]
+        elif x.status == FriendStatusEnum.PENDING:
+            if cur_user is x.user1: # Current user sent the request
+                buttons = [(f'/remove-friend/{oth_user.id}', 'Unsend')]
+            else: # Other user sent the request
+                buttons = [(f'/add-friend/{oth_user.id}', 'Approve'), (f'/remove-friend/{oth_user.id}', 'Reject')]
+        else:
+            abort(404, f'Unknown status {x.status}')
+        friends.append((oth_user, x.status, buttons))
     return render_template("my-friends.html", friends=friends)
 
 
@@ -115,7 +133,20 @@ def add_friend():
     search_form = SearchForm()
     if search_form.validate_on_submit():
         search_str = search_form.text.data
-        found_users = User.query.filter(User.username.contains(search_str) & (User.id != (current_user.get_id()))).all()
+        result = User.query.filter(User.username.contains(search_str) & (User.id != (current_user.get_id()))).all()
+        for user in result:
+            status, _ = get_friend_status(current_user.get_id(), user.id)
+            if status == 'friend':
+                buttons = [(f'/remove-friend/{user.id}', 'Remove Friend')]
+            elif status == 'pending-sent-request':
+                buttons = [(f'/remove-friend/{user.id}', 'Unsend')]
+            elif status == 'pending-to-approve':
+                buttons = [(f'/add-friend/{user.id}', 'Approve'), (f'/remove-friend/{user.id}', 'Reject')]
+            elif status == 'neutral':
+                buttons = [(f'/add-friend/{user.id}', 'Add Friend')]
+            else:
+                abort(404, description=f'Unknown status {status}')
+            found_users.append((user.username, buttons))
     return render_template("add-friend.html", search_form=search_form, found_users=found_users)
 
 
@@ -125,36 +156,54 @@ def add_friend_userid_provided(user_id):
     # Abort if adding self as friend
     if int(current_user.get_id()) == user_id:
         return abort(404, description="Cannot add yourself as friend")
-    # Get and Check if has pending friend request or already a friend
-    friend_record = db.session.query(Friend)\
-                    .filter(((Friend.user1_id == current_user.get_id()) & (Friend.user2_id == user_id))\
-                            | ((Friend.user2_id == current_user.get_id()) & (Friend.user1_id == user_id))
-                    ).one_or_none()
-    # If found friend record
-    if friend_record:
-        if friend_record.status == FriendStatusEnum.FRIEND:
-            pass # If already a friend, do nothing
-        elif friend_record.status == FriendStatusEnum.PENDING:
-            # If pending friend request
-            if friend_record.user1.id == int(current_user.get_id()): 
-                pass # If current_user sent the request, do nothing
-                flash(f'Friend request already sent to "{friend_record.user2.username}"')
-            else: # The other user sent the request
-                # Approve the friend request (Change status from pending to approved)
-                friend_record.status = FriendStatusEnum.FRIEND
-                db.session.add(friend_record)
-                db.session.commit()
-                flash(f'Approved friend request from "{friend_record.user1.username}"')
-        else:
-            abort(404, description=f"Unknown status {friend_record.status}")
-    else:
+    status, friend_record = get_friend_status(current_user.get_id(), user_id)
+    if status == 'friend':
+        # Already a friend, do nothing
+        pass
+    elif status == 'pending-sent-request':
+        # Current user sent a request, do nothing
+        flash(f'Friend request already sent to "{friend_record.user2.username}"')
+    elif status == 'pending-to-approve':
+        # Other user sent the request, approve (Change status from pending to approved)
+        friend_record.status = FriendStatusEnum.FRIEND
+        db.session.add(friend_record)
+        db.session.commit()
+        flash(f'Approved friend request from "{friend_record.user1.username}"')
+    elif status == 'neutral':
         # No friendship record found, send friend request
         user = User.query.filter_by(id=user_id).one()
         friend = Friend(user1_id=current_user.get_id(), user2_id=user.id, status=FriendStatusEnum.PENDING)
         db.session.add(friend)
         db.session.commit()
         flash(f'Sent friend request to "{user.username}"')
+    else:
+        abort(404, description=f"Unknown status {status}")
     return redirect(url_for("show_friends"))
+
+
+@myapp_obj.route("/remove-friend/<int:user_id>", methods=['GET', 'POST'])
+@login_required
+def remove_friend_userid_provided(user_id):
+    # Abort if removing self as friend
+    if int(current_user.get_id()) == user_id:
+        return abort(404, description="Cannot remove yourself from friend")
+    status, friend_record = get_friend_status(current_user.get_id(), user_id)
+    if friend_record:
+        other_user = friend_record.user1.username if friend_record.user1.id != int(current_user.get_id()) else friend_record.user2.username
+        if status == 'friend':
+            flash(f'Removed "{other_user}" from friend')
+        elif status == 'pending-sent-request':
+            flash(f'Unsent friend request to "{other_user}"')
+        elif status == 'pending-to-approve':
+            flash(f'Rejected friend request from "{other_user}"')
+        elif status == 'neutral':
+            pass # Do nothing
+        else:
+            abort(404, description=f'Unknown status {status}')
+        db.session.delete(friend_record)
+        db.session.commit()
+    return redirect(url_for("show_friends"))
+
 
 #Pomodoro app
 @myapp_obj.route("/pomodoro")

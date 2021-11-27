8efbab8720fac0d01fedbbe6e16b05aaffcd1ea6
abdullahw1 <90009221+abdullahw1@users.noreply.github.com>
2021-11-26 20:03:17 -0800

    Update __init__.py

--- app/myapp/__init__.py
+++ app/myapp/__init__.py
@@ -2,6 +2,8 @@ import flask
 import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_pagedown import PageDown
+
 
 # gives current directory of this file
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -17,5 +19,7 @@ myapp_obj.config.from_mapping(
 db = SQLAlchemy(myapp_obj)
 login = LoginManager(myapp_obj)
 login.login_view = 'login'
+pagedown = PageDown(myapp_obj)
+
 
 from myapp import routes, models

db7b3551a08e7c9cf85f083a2879b1a090209a95
abdullahw1 <90009221+abdullahw1@users.noreply.github.com>
2021-11-28 22:37:49 -0800

    database table for notes

--- app/myapp/models.py
+++ app/myapp/models.py
@@ -19,7 +19,27 @@ class User(UserMixin, db.Model):
     
     def __repr__(self):
         return f'<User {self.id}: {self.username}, {self.password}>'
+  
 
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+   
+
+   
+class Notes(db.Model):
+    """Database table for notes
+
+     Attributes:
+         id: Primary key
+         title: String column, title of note
+         data: text column, containing files data
+         User: id if user who added notes
+     """
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    data = db.Column(db.Text)
+    User = db.Column(db.Integer, db.ForeignKey('user.id'))
+    def __repr__(self):
+        return f'<{self.name}   {self.data}>'

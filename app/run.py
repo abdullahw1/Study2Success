c301f8adce8ba5ee078254550963813032a40144
HoaTNNguyen <hoatuongnguyen.nguyen@sjsu.edu>
2021-12-05 22:48:48 -0800

    Don't specify port number

--- app/run.py
+++ app/run.py
@@ -4,7 +4,6 @@ import webbrowser
 from myapp import myapp_obj, db
 
 DEBUG = False
-PORT_NUMBER = 5000
 
 def launch_browser():
     webbrowser.open(f'http://localhost:{PORT_NUMBER}', new=0)
@@ -18,4 +17,4 @@ db.create_all()
 #    threading.Timer(1, launch_browser).start()
 
 # Run flask app server
-myapp_obj.run(debug=DEBUG, port=PORT_NUMBER)
+myapp_obj.run(debug=DEBUG)
